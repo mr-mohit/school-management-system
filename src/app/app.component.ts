0e1d387ef24486e0a569a2f20cfe93b0254e2926
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2019-02-21 12:03:15 +0530

    Fixed StatusBar Issue and button position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleBlackOpaque();
       splashScreen.hide();
 
     });
