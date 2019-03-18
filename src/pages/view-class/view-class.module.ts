aa26fa9a11ba22a5d83ad736ae828d2237663264
emmy29 <41987378+emmy29@users.noreply.github.com>
2019-03-18 12:01:30 +0530

    view subject page

---
+++ src/pages/view-class/view-class.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ViewClassPage } from './view-class';
+
+@NgModule({
+  declarations: [
+    ViewClassPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ViewClassPage),
+  ],
+})
+export class ViewClassPageModule {}
---
+++ src/pages/view-term/view-term.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ViewTermPage } from './view-term';
+
+@NgModule({
+  declarations: [
+    ViewTermPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ViewTermPage),
+  ],
+})
+export class ViewTermPageModule {}
