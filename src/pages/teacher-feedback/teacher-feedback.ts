75323a3ec2d585c811054ccf561e5ef37a730aa3
Kainth Sourav <souravkainth@outlook.com>
2019-04-01 14:50:56 +0530

    Update teacher-feedback.ts

--- src/pages/teacher-feedback/teacher-feedback.ts
+++ src/pages/teacher-feedback/teacher-feedback.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ServiceAddStudentfeedbackProvider } from '../../providers/service-add-studentfeedback/service-add-studentfeedback';
 import { ServiceGetClassMasterProvider } from '../../providers/service-get-class-master/service-get-class-master';
 import { FeedbackDetailPage } from '../feedback-detail/feedback-detail';
 
