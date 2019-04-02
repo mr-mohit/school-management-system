16073ad0523c6bcde0a2a56e523b781b6bf1c3c7
Kainth Sourav <souravkainth@outlook.com>
2019-04-02 11:50:14 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,7 @@ import { NativeStorage } from '@ionic-native/native-storage';
 import { Push, PushObject, PushOptions } from '@ionic-native/push';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import { SchoolInfoPage } from '../pages/school-info/school-info';
+import { ViewCalendarPage } from '../pages/view-calendar/view-calendar';
 
 
 @Component({
@@ -106,7 +107,7 @@ export class MyApp {
     this.Student_a=[
       {title:'Home', icon:'home', component:StudentdashboardPage},
       {title:'School Info', icon:'contact', component:SchoolInfoPage},
-      {title:'Events', icon:'contact',component:ViewEventsPage},
+      {title:'Events', icon:'contact',component:ViewCalendarPage},
       {title:'Feedback', icon:'contact',component:StudentFeedbackPage},
       {title:'Quiz', icon:'contact', component:StudentQuizPage},
       {title:'Gallery',icon:'images',component:GalleryPage}
@@ -119,7 +120,7 @@ export class MyApp {
       {title:'Announcements',icon:'megaphone',component:TeacherAnnouncementPage},
       {title:'Feedback',icon:'contact',component:TeacherFeedbackPage},      
       {title:'Gallery',icon:'images',component:GalleryPage},
-      {title:'Events', icon:'contact',component:ViewEventsPage}
+      {title:'Events', icon:'contact',component:ViewCalendarPage}
     ];
     //initializing the Admin array elements for side menu
     this.Admin_a=[
