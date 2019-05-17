392ddfc213d24a0938735c943334a0a7a0c4e854
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2019-05-17 15:55:03 +0530

    ...

--- src/providers/service-login/service-login.ts
+++ src/providers/service-login/service-login.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class ServiceLoginProvider {
   public data:any;
     public URL="http://ftp.cpckingdom.com/easyschool.cpckingdom.com/schoolapi/"; //for server use
-    //public URL="http://localhost/schoolapi/"; //for local use
+    //public URL="http://172.26.17.166/schoolapi/"; //for local use
     //public URL="https://direct-school.000webhostapp.com/"; //for hosting
   
   public recdata: any;//this variable will store info coming from API
